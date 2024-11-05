c608809324c3cc4cff6920d6507f1749f2989639
andihasan97 <andihasanelfalakiy@gmail.com>
2024-11-05 16:26:19 +0700

    add: example

--- app/src/main/java/com/andihasan7/flashbarsample/MainActivity.kt
+++ app/src/main/java/com/andihasan7/flashbarsample/MainActivity.kt
@@ -20,8 +20,10 @@ package com.andihasan7.flashbarsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.Typeface
+import android.widget.ImageView.ScaleType
 import com.andihasan7.flashbarsample.databinding.ActivityMainBinding
 import com.andihasan7.flashbar.Flashbar
+import com.andihasan7.flashbar.anim.FlashAnim
 
 public class MainActivity : AppCompatActivity() {
 
@@ -100,6 +102,72 @@ public class MainActivity : AppCompatActivity() {
             fl?.show()
         }
         
+        binding.btnOverlay.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = overlay()
+            }
+            fl?.show()
+        }
+        
+        binding.btnActionPrimary.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = actionPrimary()
+            }
+            fl?.show()
+        }
+        
+        binding.btnPlusminus.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = plusMinus()
+            }
+            fl?.show()
+        }
+        
+        binding.btnIcon.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = icon()
+            }
+            fl?.show()
+        }
+        
+        binding.btnProgress.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = progress()
+            }
+            fl?.show()
+        }
+        
+        binding.btnEnterExitVertical.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = enterExitVertical()
+            }
+            fl?.show()
+        }
+        
+        binding.btnEnterExitHorizontal.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = enterExitHorizontal()
+            }
+            fl?.show()
+        }
+        
+        binding.btnAnimIcon.setOnClickListener {
+            var fl: Flashbar? = null
+            if (fl == null) {
+                fl = animationIcon()
+            }
+            fl?.show()
+        }
+        
+        
+        
         
     }
     
@@ -164,6 +232,138 @@ public class MainActivity : AppCompatActivity() {
                 .duration(1500)
                 .message("You can have gradients by setting background drawable.")
                 .backgroundDrawable(R.drawable.bg_gradient)
+                .build()
+    }
+    
+    private fun overlay(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(1500)
+                .message("You can show flashbar with overlay.")
+                .showOverlay()
+                //.overlayColorRes(R.color.modal)
+                //.overlayBlockable()
+                .build()
+    }
+    
+    private fun actionPrimary(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .title("Hello World!")
+                //.backgroundColorRes(R.color.colorPrimaryDark)
+                .message("You can click on the primary action button.")
+                .primaryActionText("TRY NOW")
+                .primaryActionTextColorRes(R.color.green)
+                .primaryActionTextSizeInSp(20f)
+                .primaryActionTapListener(object : Flashbar.OnActionTapListener {
+                        override fun onActionTapped(bar: Flashbar) {
+                            bar.dismiss()
+                        }
+                    })
+                .build()
+    }
+    
+    private fun plusMinus(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .title("Hello World!")
+                .message("You can show either or both of the positive/negative buttons and customize them similar to the primary button.")
+                //.backgroundColorRes(R.color.colorPrimaryDark)
+                .positiveActionText("YES")
+                .negativeActionText("NO")
+                .positiveActionTextColorRes(R.color.green)
+                .negativeActionTextColorRes(R.color.green)
+                .positiveActionTapListener(object : Flashbar.OnActionTapListener {
+                        override fun onActionTapped(bar: Flashbar) {
+                            bar.dismiss()
+                        }
+                    })
+                .negativeActionTapListener(object : Flashbar.OnActionTapListener {
+                        override fun onActionTapped(bar: Flashbar) {
+                            bar.dismiss()
+                        }
+                    })
+                .build()
+    }
+    
+    private fun icon(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(1500)
+                .title("Hello World!")
+                .message("You can show a default icon on the left side of the withView")
+                //.backgroundColorRes(R.color.colorPrimaryDark)
+                .showIcon(0.8f, ScaleType.CENTER_CROP)
+                //.showIcon() default ic_info
+                //.icon(R.drawable.ic_settings) you can custom icon
+                //.iconColorFilterRes(R.color.colorAccent)
+                .build()
+    }
+    
+    private fun progress(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(1500)
+                .title("Hello World!")
+                .message("You can show the progress bar on either the left or right side of the view")
+                .showProgress(Flashbar.ProgressPosition.LEFT) // RIGHT
+                .progressTintRes(R.color.green)
+                .build()
+    }
+    
+    private fun enterExitVertical(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(1500)
+                .title("Hello World!")
+                .message("You can change the enter/exit animation vertically")
+                //.backgroundColorRes(R.color.colorPrimaryDark)
+                .enterAnimation(FlashAnim.with(this)
+                    .animateBar()
+                    .duration(750)
+                    .alpha()
+                    .overshoot())
+                .exitAnimation(FlashAnim.with(this)
+                    .animateBar()
+                    .duration(400)
+                    .accelerateDecelerate())
+                .build()
+    }
+    
+    private fun enterExitHorizontal(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(1500)
+                .title("Hello World!")
+                .message("You can change the enter/exit animation horizontally")
+                .enterAnimation(FlashAnim.with(this)
+                        .animateBar()
+                        .duration(400)
+                        .slideFromRight() // .slideFromLeft
+                        .overshoot())
+                .exitAnimation(FlashAnim.with(this)
+                        .animateBar()
+                        .duration(250)
+                        .slideFromLeft() // .slideFromRight
+                        .accelerate())
+                .build()
+    }
+    
+    private fun animationIcon(): Flashbar {
+        return Flashbar.Builder(this)
+                .gravity(Flashbar.Gravity.TOP)
+                .duration(2500)
+                .title("Hello World!")
+                .message("You can make an icon as animation.")
+                .showIcon()
+                //.icon(R.drawable.ic_drop) custom icon
+                .iconColorFilterRes(R.color.red)
+                .iconAnimation(FlashAnim.with(this)
+                        .animateIcon()
+                        .pulse()
+                        .alpha()
+                        .duration(750)
+                        .accelerate())
                 .build()
     }
     
